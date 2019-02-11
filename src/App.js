1823acd0db302d80bb3f402500295602f50fcecf
torikulislam024 <torikulislam024@gmail.com>
2019-02-11 19:23:54 +0600

    streaming using web rtc and capture screen

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+  
+  
+  capture=()=>{
+    document.getElementById('capture').addEventListener('click',  ()=> {
+        document.getElementById('canvas').getContext('2d').drawImage(document.getElementById('player'), 0, 0, 640, 480)
+    })
+}
+
+render() {
+        navigator.getUserMedia = navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia;
+
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia({video: true},
+            (stream) => {
+                let video = document.getElementById('player');
+                video.srcObject = stream;
+                video.onloadedmetadata = (e) => {
+                    video.play();
+
+                };
+            }, (err) => {
+                alert(err.message);
+
+            }
+        );
+    } else {
+        console.log("getUserMedia not supported");
+    }
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div>
+            <video id='player' width="640" height="480" autoPlay/>
+            <button id="capture" onClick={this.capture}>Capture</button>
+            <canvas id="canvas" width="380" height="280"></canvas>
+        </div>
+
     );
-  }
+}
 }
 
 export default App;
